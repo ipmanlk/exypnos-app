26c838fa7a1f498f3662a5bf25d6cf21398e2f10
ipmanlk <navilk@zoho.com>
2018-10-19 13:07:10 +0530

    fix: merge issues

--- www/js/app/common.js
+++ www/js/app/common.js
@@ -8,8 +8,9 @@ function onOffline() {
 // override back button
 document.addEventListener("backbutton", function (e) {
   e.preventDefault();
-  if (currentPage !== "index") {
-    window.location = "index.html";
+  if (currentPage !== "postList") {
+    currentPage = "postList";
+    showPostList();
   } else {
     exitApp();
   }
@@ -17,6 +18,23 @@ document.addEventListener("backbutton", function (e) {
 
 function exitApp() {
   navigator.app.exitApp();
+}
+
+function showPostList() {
+  $('#postContent').hide();
+  $('#postList').fadeIn();
+  $(window).scrollTop(($(lastPostID).offset().top) - 80);
+}
+
+function loadCat(catID,catName) {
+  $('#navBarBtn').click();
+  catFilter = true;
+  getPosts(0, catID);
+  $('#content').fadeOut();
+  $('#posts').empty();
+  $('#catMsg').show();
+  $('#catName').text(catName);
+  $('#loadMorePostsBtn').fadeIn();
 }
 
 // get categories
--- www/js/app/index.js
+++ www/js/app/index.js
@@ -1,15 +1,22 @@
 // define where are you
-var currentPage = "index";
+var currentPage = "postList";
+// global post title for sharing
+var postTitle;
+// remember last post id to scroll back
+var lastPostID;
 
 // browse by category or not
 var catFilter = false;
 
-$(document).ready(function() {
+// check if device is ready or not
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
   $('#navBar').load('navBar.html');
   $("#loader").fadeIn();
   getPosts("0");
   getCats();
-});
+}
 
 function getPosts(id,catID='') {
   var url;
@@ -51,7 +58,7 @@ function getPosts(id,catID='') {
       }
       // show posts
       $("#loadMorePostsMsg").hide();
-      $("#content").fadeIn();
+      $("#postList").fadeIn();
       $("#loader").hide();
     }
   });
@@ -63,7 +70,7 @@ function getCard(post) {
   title = post.title;
   coverImg = post.cover_img;
   shortDes = post.short_des;
-  var html = '<div class="container mt-4"><div class="card text-white bg-dark mb-3" style="max-width: 100%;"><img class="card-img-top" src="' + coverImg + '" alt="Card image cap"><div class="card-body"><h5 class="card-title sinhala"><strong>' + title + '</strong></h5><p class="card-text sinhala">' + shortDes + '</p><a href="#" class="btn btn-primary btn-block" onclick="' + "goToPost('" + id + "')"+ '">Read More</a></div></div></div></div>';
+  var html = '<div id="' + id + '" class="container mt-4"><div class="card text-white bg-dark mb-3" style="max-width: 100%;"><img class="card-img-top" src="' + coverImg + '" alt="Card image cap"><div class="card-body"><h5 class="card-title sinhala"><strong>' + title + '</strong></h5><p class="card-text sinhala">' + shortDes + '</p><a href="#" class="btn btn-primary btn-block" onclick="' + "showPost('" + id + "')"+ '">Read More</a></div></div></div></div>';
   return(html)
 }
 
@@ -76,10 +83,6 @@ function appendToPosts(html,position) {
 
 }
 
-function goToPost(id) {
-  window.location = "./post.html?id=" + id;
-}
-
 function loadMorePosts() {
 
   $("#loadMorePostsMsg").fadeIn();
@@ -89,7 +92,6 @@ function loadMorePosts() {
   var oldestPostId = Object.keys(postData).length -1;
   // get the post id of last item
   oldestPostId = postData[oldestPostId].post_id;
-  // if it's not 1, request for more posts
 
   // if category filter is enabled
   if (catFilter) {
@@ -104,13 +106,36 @@ function getJsonPostData() {
   return(JSON.parse(localStorage.getItem('postData')));
 }
 
-function loadCat(catID,catName) {
-  $('#navBarBtn').click();
-  catFilter = true;
-  getPosts(0, catID);
-  $('#content').fadeOut();
-  $('#posts').empty();
-  $('#catMsg').show();
-  $('#catName').text(catName);
-  $('#loadMorePostsBtn').fadeIn();
+function showPost(id) {
+  currentPage = "post";
+  loadPost(id);
+  $('#postList').hide();
+  $('#postContent').fadeIn();
+  lastPostID =  "#" + id;
+}
+
+function loadPost(id) {
+  var postData = getJsonPostData();
+  for (post in postData) {
+    if (postData[post].post_id == id) {
+      $("#title").html(postData[post].title);
+      postTitle = postData[post].title;
+      $("#datatime").html(postData[post].datetime);
+      $("#coverimg").attr("src", postData[post].cover_img);
+      $("#post").html(postData[post].post);
+      $("#cardimg1").attr("src", postData[post].card_img1);
+      $("#cardimg2").attr("src", postData[post].card_img2);
+      $("#author").html(postData[post].author);
+      $("#authorInfo").html(postData[post].author_info);
+    }
+  }
+  fixElementSizes();
+}
+
+function fixElementSizes() {
+  $("iframe").width('100%');
+}
+
+function sharePost() {
+  window.plugins.socialsharing.share(postTitle + " - Readmore @ ", null, null, "Exypnos Android App.");
 }
