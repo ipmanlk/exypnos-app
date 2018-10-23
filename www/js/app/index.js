c04c9713c471754df745916052889e3c7c27e130
ipmanlk <navilk@zoho.com>
2018-10-24 01:07:50 +0530

    fix: infinite post load

--- www/js/app/index.js
+++ www/js/app/index.js
@@ -56,7 +56,7 @@ function getPosts(id,catID) {
       } else {
         // enable load more
         loadMore = true;
-        
+
         if (catFilter) {
           $('#catMsg').fadeIn();
         }
@@ -148,6 +148,7 @@ function sharePost() {
 }
 
 function showPostList() {
+  currentPage = "postList";
   $('#postContent, #catContent').hide();
   $('#postList').fadeIn();
   $(window).scrollTop(($(lastPostID).offset().top) - 80);
@@ -216,7 +217,7 @@ function onOnline() {
 // detect scroll
 $(window).scroll(function () {
   if ($(document).height()-100 <= $(window).scrollTop() + $(window).height()) {
-    if (loadMore !== "disabled" && loadMore == true) {
+    if (loadMore !== "disabled" && loadMore == true && currentPage=="postList") {
       loadMorePosts();
     }
   }
