b88544e4c8d03d09694e3f88bba1f88b23bcde85
ipmanlk <navilk@zoho.com>
2018-10-27 21:37:23 +0530

    Revert "add: basic fav sync (garbage)"
    This reverts commit d7a123228851ac8ecd9042d0947cd3ea335aed16.

--- www/js/app/fav.js
+++ www/js/app/fav.js
@@ -6,44 +6,17 @@ function favAdd() {
   } else {
     favs = JSON.parse(localStorage.getItem('favs'));
   }
-var suser_code = localStorage.getItem('suser_code');
+
   if (favs[currentPostID] == null) {
     favs[currentPostID] = postsList[currentPostID];
     $('#postFavIcon').removeClass('far fa-star');
     $('#postFavIcon').addClass('fas fa-star');
     showToast("Favorite saved!", "This post can be found in your favorites.","success", 2000);
-
-
-    $.ajax({
-      type: 'get',
-      url: "https://exypnos.navinda.xyz/api/v2/t.php?s=4a2204811369&fav_a=0&suser_code=" + suser_code + "&post_id=" + currentPostID,
-      dataType: 'json',
-      timeout: 60000, //60s
-      success: function (data) {
-        if (data == "1") {
-          alert("added!");
-        }
-      }
-    });
-
   } else {
     delete favs[currentPostID];
     $('#postFavIcon').removeClass('fas fa-star');
     $('#postFavIcon').addClass('far fa-star');
     showToast("Favorite removed!", "This post can no longer be found in your favorites.","success", 2000);
-
-
-    $.ajax({
-      type: 'get',
-      url: "https://exypnos.navinda.xyz/api/v2/t.php?s=4a2204811369&fav_d=0&suser_code=" + suser_code + "&post_id=" + currentPostID,
-      dataType: 'json',
-      timeout: 60000, //60s
-      success: function (data) {
-        if (data == "1") {
-          alert("deleted!");
-        }
-      }
-    });
   }
 
   $('#postFavBtn').blur();
@@ -83,22 +56,4 @@ function favCheck() {
     $('#postFavIcon').removeClass('fas fa-star');
     $('#postFavIcon').addClass('far fa-star');
   }
-}
-
-function favRestore() {
-  var suser_code = localStorage.getItem('suser_code');
-  $.ajax({
-    type: 'get',
-    url: "https://exypnos.navinda.xyz/api/v2/t.php?s=4a2204811369&fav_g=0&suser_code=" + suser_code + "&post_id=" + currentPostID,
-    dataType: 'json',
-    timeout: 60000, //60s
-    success: function (data) {
-      if (data == "1") {
-        showToast("Post Liked!", "Thanks for giving your feedback.","success", 2000);
-        $('#postLikeIcon').removeClass("far fa-thumbs-up");
-        $('#postLikeIcon').addClass("fas fa-thumbs-up");
-        $('#postLikes').text(parseInt($('#postLikes').text()) + 1);
-      }
-    }
-  });
 }
